3f0610881cd00238dc813ed971e4d9acaf7df02f
skloqman <135141216+skloqman@users.noreply.github.com>
2026-07-06 19:38:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import sqlite3
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 DB_FILE = "/tmp/database.db"
 
 BASE_ROSTER = [
